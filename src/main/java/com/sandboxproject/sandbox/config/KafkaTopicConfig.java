764e14ac26302bf1d8b9b4591c74aa16c93b09ab
Josip Tomasevic <jtomasevic82@gmail.com>
2024-01-17 13:08:40 +0100

    Initial setup with some Kafka configuration

---
+++ src/main/java/com/sandboxproject/sandbox/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.sandboxproject.sandbox.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic buildTopic() {
+        return TopicBuilder.name("testTopic")
+                .build();
+    }
+}
